97766529d05ee6a93ed3a5267b557968d7c2ed5c
Hunter <hcobrian@gmail.com>
2014-09-28 15:32:10 -0700

    Green button visible functionality added
    Made the green button actually do what it says it will by finding the
    col.teammates class div and mapping the promised data to the HTML
    inside.

--- assets/js/my_app.js
+++ assets/js/my_app.js
@@ -15,9 +15,22 @@ $( document ).ready(function() {
 
     $( "button.toggle-teammate-meta" ).on( "click", function() {
         console.log("Fetching teammate data");
-        var data_that_was_fetched = fetch_teammates_details();
+        var data_that_was_fetched = fetch_teammates_details();        
         $.when( data_that_was_fetched ).then(function( our_promised_data ) {
             console.log("data_that_was_fetched from details:", our_promised_data);
+        
+        var teammate_wrapper = $(".col.teammate");
+        if (teammate_wrapper){
+            // teammate_wrapper.html("");
+            var full_name = our_promised_data.first_name + " " + our_promised_data.last_name;
+            var full_name_h2 = teammate_wrapper.find("h2");
+            full_name_h2.html(full_name);
+            var hobbies = our_promised_data.hobbies;
+            var hobbies_p = teammate_wrapper.find("p");
+            hobbies_p.html(hobbies);
+
+        }
+
         });
     });
 
@@ -28,6 +41,7 @@ $( document ).ready(function() {
         })
         .done(function( data, textStatus, jqXHR ) {
             console.log( "second success", data );
+        
         })
         .fail(function( jqXHR, textStatus, errorThrown ) {
             console.log( "error" );
@@ -35,8 +49,8 @@ $( document ).ready(function() {
         .always(function( data, textStatus, errorThrown ) {
             console.log( "complete" );
         });
-
         return our_promised_data;
+
 
     };
 
